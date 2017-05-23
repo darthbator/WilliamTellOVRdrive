bc8b5590070bd5a1e22904d3971e002a96b03786
David Bator <darthbator@gmail.com>
2017-05-22 19:20:01 -0700

    adding apple hit effects

--- William Tell Simulator/Assets/Scripts/Gun.cs
+++ William Tell Simulator/Assets/Scripts/Gun.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour {
     public Transform apple;
+	public Transform head;
     public Transform firingTrans;
     public float minDistance;
     private float distance;
@@ -24,12 +25,16 @@ public class Gun : MonoBehaviour {
         if (!canFire)
             return;
 
+		Debug.DrawRay(firingTrans.position, firingTrans.forward * 10000f, Color.red, 5f);
+
         RaycastHit hit;
         if (Physics.Raycast(firingTrans.position, firingTrans.forward, out hit)) {
-			if (hit.transform == apple)
+			if (hit.transform == apple) {
 				Debug.Log("Apple core!");
-			else
-				Debug.Log("shot child!");
+				apple.GetComponent<Renderer>().material.color = Color.green;
+			} else if (hit.transform == head)
+				Debug.LogError("You shot your son!!!");
+				
         }
     }
 
